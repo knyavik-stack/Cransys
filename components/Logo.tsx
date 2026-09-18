02296fa6982e9ba5cb581774cb695f1b9a75227f
knyavik <knyavik@gmail.com>
2026-09-18 18:40:26 +0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -15,17 +15,6 @@ export function Logo({ className = '', size = 32 }: LogoProps) {
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
-      {/* Инженерная чертёжная сетка Blueprint */}
-      <g stroke="#7EA8D3" strokeWidth="6" opacity="0.65" strokeLinecap="round">
-        <line x1="100" y1="160" x2="430" y2="160" />
-        <line x1="100" y1="256" x2="430" y2="256" />
-        <line x1="100" y1="352" x2="430" y2="352" />
-        
-        <line x1="160" y1="100" x2="160" y2="430" />
-        <line x1="256" y1="100" x2="256" y2="430" />
-        <line x1="352" y1="100" x2="352" y2="430" />
-      </g>
-
       {/* Каркас и контуры буквы C */}
       <g stroke="#003882" strokeWidth="22" strokeLinecap="round" strokeLinejoin="round" fill="none">
         {/* Внешний контур C */}
